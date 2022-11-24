b660619ce8d9a0dcad10bdc3224e575c41eb91ef
akaruchy <katia9@bk.ru>
2022-11-24 12:19:30 +0300

    4

---
+++ homework/lesson4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает 
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+int x = 1;
+
+while (x<=n)
+{
+    if (x%2==0)
+        Console.WriteLine(x);
+    x+=1;
+}
+
