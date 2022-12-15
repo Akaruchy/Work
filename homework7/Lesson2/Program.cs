0acad52d6b017e3403d6c916c284470cfe06f5b0
akaruchy <katia9@bk.ru>
2022-12-15 18:31:11 +0300

    les7

---
+++ homework7/Lesson2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 2: Напишите программу, которая на вход
+//принимает позиции элемента в двумерном массиве, и
+//возвращает значение этого элемента или же указание,
+//что такого элемента нет.
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] array = new int[row, column];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = new Random().Next(0, 100);
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + " ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите координаты искомого элемента:");
+{
+    Console.WriteLine("Позиция в строке:");
+    int x = int.Parse(Console.ReadLine());
+    Console.WriteLine("Позиция в столбце:");
+    int y = int.Parse(Console.ReadLine());
+    if ((x > row) || (y > column))
+        Console.WriteLine("такого числа нет");
+    else
+    {
+        int coordinates = array[x, y];
+        Console.WriteLine(coordinates);
+    }
+}
+
---
+++ homework7/Lesson3/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 3: Задайте двумерный массив из целых чисел.
+//Найдите среднее арифметическое элементов в каждом
+//столбце.
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] array = new int[row, column];
+
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = new Random().Next(0, 10);
+        
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + " ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Среднее арифметическое:");
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        avarage = (avarage + array[i, j]);
+    }
+    avarage = avarage/row;
+    Console.Write(avarage + "; ");
+}
+
+
+
