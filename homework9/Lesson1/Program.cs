aba84e279227a70ccb698a08f3e1deeb18ed8c2e
akaruchy <katia9@bk.ru>
2022-12-23 18:48:36 +0300

    end

---
+++ homework9/Lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все
+//чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+
+Console.WriteLine("Задайте значение M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте значение N:");
+int n = int.Parse(Console.ReadLine());
+
+void Numbers (int m, int n)
+{
+    if (m > n) return;
+    Numbers (m+1, n);
+    if (m % 2 != 0) return;
+    Console.WriteLine($"{m}");
+}
+Numbers(m,n);
---
+++ homework9/Lesson2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+Console.WriteLine("Задайте значение M:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте значение N:");
+int n = int.Parse(Console.ReadLine());
+int Sum = 0;
+
+Console.WriteLine("Так, так, так, вычисляю:");
+Console.WriteLine("Еще минуточку....");
+Console.WriteLine("Воооот так:");
+
+void Numbers (int m, int n)
+{
+    if (m > n) return;
+    Numbers (m+1, n);
+    Sum = Sum + m;
+    Console.Write($"+{m}");
+}
+Numbers(m,n);
+Console.WriteLine();
+Console.WriteLine($"Сумма натуральных элементов:{Sum}");
+
+//  Спасибо за уроки, Мария, это было очень здорово! И увлекательно!
